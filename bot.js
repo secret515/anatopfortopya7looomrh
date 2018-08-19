3bf80d163ff7b72ddba158ab8bccfa6206a552c5
secret515 <41595419+secret515@users.noreply.github.com>
2018-08-19 07:50:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 ///hug
 client.on('message', message => {
- if(message.content.startsWith('hug')) {
+ if(message.content.startsWith(!+'hug')) {
    let args = message.content.split(" ").slice(1);
     let hug = [
         "https://78.media.tumblr.com/28e86413485a973ae859a745a7a5382e/tumblr_o1hf0uK2NL1uapp8co1_500.gif",
@@ -56,7 +56,7 @@ client.on('message', message => {
 
 ///kiss
 client.on('message', message => {
- if(message.content.startsWith('kiss')) {
+ if(message.content.startsWith(!+'kiss')) {
    let args = message.content.split(" ").slice(1);
     let hug = [
         "https://media.giphy.com/media/KH1CTZtw1iP3W/giphy.gif",
@@ -111,7 +111,7 @@ client.on('message', message => {
 
 ///slap
 client.on('message', message => {
- if(message.content.startsWith( 'slap' )) {
+ if(message.content.startsWith( !+'slap' )) {
    let args = message.content.split(" ").slice(1);
     let hug = [
         "https://cdn.discordapp.com/attachments/466949822604247044/467005787189805066/Slap_6.gif",
@@ -165,7 +165,7 @@ client.on('message', message => {
 
 ///pat
 client.on('message', message => {
- if(message.content.startsWith(prefix + 'pat')) {
+ if(message.content.startsWith(! + 'pat')) {
    let args = message.content.split(" ").slice(1);
     let hug = [
         "https://cdn.discordapp.com/attachments/466949822604247044/467186871290822657/pat-B1SOzCV0W.gif",
@@ -219,7 +219,7 @@ client.on('message', message => {
 
 ///cuddle
 client.on('message', message => {
- if(message.content.startsWith(prefix + 'cuddle')) {
+ if(message.content.startsWith(! + 'cuddle')) {
    let args = message.content.split(" ").slice(1);
     let hug = [
         "https://cdn.discordapp.com/attachments/466949822604247044/467005578548346900/Cuddle6.gif",
@@ -273,7 +273,7 @@ client.on('message', message => {
 
 ///poke
 client.on('message', message => {
- if(message.content.startsWith(prefix + 'poke')) {
+ if(message.content.startsWith(! + 'poke')) {
    let args = message.content.split(" ").slice(1);
     let hug = [
         "https://cdn.discordapp.com/attachments/467499708068265994/467507303432585227/poke-rJzUe1FwZ.gif",
